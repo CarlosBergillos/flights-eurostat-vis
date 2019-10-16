eaa82ade2884afb34fa70b8308d3bbedb3bfcfb7
Carlos Bergillos <carlos98.air.cn@gmail.com>
2019-10-16 22:31:25 +0300

    update

--- evolution_parse_airports.py
+++ evolution_parse_airports.py
@@ -9,6 +9,23 @@ file_in = './avia_par_es/avia_par_es_fixed.csv'
 df = pd.read_csv(file_in, sep=',')
 departure_airports = df['airpt_dep'].unique()
 
+def evolution_all(timeperiods, units):
+    data_from_airport = df
+    result = pd.DataFrame()
+    file_out = f'./custom_data/ALL.csv'
+    for unit in units:
+      unit2 = units_map[unit]
+      data_from_airport_unit = data_from_airport[data_from_airport['tra_meas']==unit]
+      sums_from_airport_unit = data_from_airport_unit[timeperiods].sum()
+      sums_from_airport_unit = sums_from_airport_unit.to_frame(unit2)
+      if result.empty:
+        result = sums_from_airport_unit
+      else:
+        result = pd.concat([result, sums_from_airport_unit], axis=1)
+    print(result)
+    result.to_csv(file_out, index=True, index_label='timeperiod', sep=',')
+
+
 def parse_airport_data(airport, timeperiods, units):
     data_from_airport = df[df['airpt_dep']==airport]
     result = pd.DataFrame()
@@ -33,7 +50,8 @@ if __name__ == '__main__':
 
     units = ["PAS_CRD", "CAF_PAS"]
 
-    for airport in departure_airports:
-    # for airport in ["LEBL"]:
-        print(f"{airport}")
-        parse_airport_data(airport, timeperiods, units)
+    # for airport in departure_airports:
+    #     print(f"{airport}")
+    #     parse_airport_data(airport, timeperiods, units)
+
+    evolution_all(timeperiods, units)
